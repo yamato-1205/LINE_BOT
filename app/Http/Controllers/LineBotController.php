dd239611e328b960ec17574428b34bfa68f5dc75
yamato-1205 <ysakata1205@gmail.com>
2024-03-15 08:16:40 +0900

    乱数生成の追加

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use LINE\Clients\MessagingApi\Configuration;
@@ -10,6 +11,7 @@ use LINE\Clients\MessagingApi\Model\ReplyMessageRequest;
 use LINE\Clients\MessagingApi\ApiException;
 use LINE\Webhook\Model\MessageEvent;
 use LINE\Webhook\Model\TextMessageContent;
+use LINE\Webhook\Model\Mention;
 use LINE\Parser\EventRequestParser;
 use Illuminate\Support\Facades\Log;
 
@@ -59,20 +61,64 @@ class LineBotController extends Controller
                 // テキストメッセージを取得
                 $eventMessageText = $eventMessage->getText();
 
-                // 文字数カウント
-                $stringCount = mb_strlen($eventMessageText);
+                // 改行コードを統一
+                $eventMessageText = str_replace(array("\r\n", "\r", "\n"), "\n", $eventMessageText);
+                // 命令文を抽出
+                $eventMessageTextArr = explode("\n", $eventMessageText, 2);
 
-                // 応答メッセージを作成
-                $message = new TextMessage([
+                if ($eventMessageTextArr[0] == "文字数" and count($eventMessageTextArr) == 2) {
+                  // 文字数カウント
+                  $stringCount = mb_strlen($eventMessageTextArr[1]);
+                  $stringSub = substr_count($eventMessageTextArr[1],"\n");
+                  // 応答メッセージを作成
+                  $message1 = new TextMessage([
                     'type' => 'text',
-                    // 'text' => $eventMessageText . "なんていうなよw",
-                    'text' => "あなたの送信したテキストの文字数は" . $stringCount . "文字です。",
-                ]);
+                    'text' => "あなたの送信したテキストの文字数は" . $stringCount - $stringSub . "文字です。",
+                  ]); 
+                  $message = [$message1];
+                } else if ($eventMessageTextArr[0] == "乱数") {
+                  // 乱数の生成
+                  try {
+                    if ( preg_match( '/^[0-9]+$/', $eventMessageTextArr[1])) {
+                      $randMax = (int)$eventMessageTextArr[1];
+                    } else {
+                      $randMax = 100;
+                    }
+                  } catch ( Exception $ex ) {
+                    $randMax = 100;
+                  }
+                  $rand = rand(0, $randMax);
+                  // 応答メッセージを作成
+                  $message1 = new TextMessage([
+                    'type' => 'text',
+                    'text' => $rand,
+                  ]); 
+                  $message2 = new TextMessage([
+                    'type' => 'text',
+                    'text' => "乱数範囲は0〜" . $randMax . "です",
+                  ]); 
+                  $message = [$message1, $message2];
+                } else {
+                  // 応答メッセージを作成
+                  $message1 = new TextMessage([
+                    'type' => 'text',
+                    'text' => "文字数\nカウントしたい文章",
+                  ]);
+                  $message2 = new TextMessage([
+                    'type' => 'text',
+                    'text' => "乱数\n乱数の最大値（半角数字）",
+                  ]);
+                  $message3 = new TextMessage([
+                    'type' => 'text',
+                    'text' => "上のように1行目に行いたいアクション。2行目にそのアクションに必要な情報を送信してください。",
+                  ]);
+                  $message = [$message1, $message2, $message3];
+                }
 
                 // 応答リクエストを作成
                 $request = new ReplyMessageRequest([
                     'replyToken' => $event->getReplyToken(),
-                    'messages' => [$message], // 配列である必要があります
+                    'messages' => $message, // 配列である必要があります
                 ]);
 
                 // 応答リクエストを送信する
