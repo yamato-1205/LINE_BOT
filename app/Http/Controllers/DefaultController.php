7e10c265b8a23584731cfbc08dfc4124a116f852
yamato-1205 <ysakata1205@gmail.com>
2024-04-30 01:28:09 +0900

    ヘルプの追加

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -8,16 +8,8 @@ class DefaultController extends Controller {
     // 応答メッセージを作成
     $message1 = new TextMessage([
       'type' => 'text',
-      'text' => "文字数\nカウントしたい文章",
+      'text' => "「ヘルプ」と送るとYAMATO_BOTの使い方や利用できるアクションの紹介をします",
     ]);
-    $message2 = new TextMessage([
-      'type' => 'text',
-      'text' => "乱数\n乱数の最大値（半角数字）",
-    ]);
-    $message3 = new TextMessage([
-      'type' => 'text',
-      'text' => "上のように1行目に行いたいアクション。2行目にそのアクションに必要な情報を送信してください。",
-    ]);
-    return [$message1, $message2, $message3];
+    return [$message1];
 	}
 }
